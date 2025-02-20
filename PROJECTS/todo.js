13d12334d6a4de881e107595be8e8d3f43db7727
Gagan Deep <gagan310.be22@chitkara.edu.in>
2025-02-20 10:53:49 +0530

    Update 1st time on 20 feb 2025

---
+++ PROJECTS/todo.js
@@ -0,0 +1,36 @@
+// 🛠️ Small Project: Interactive To-Do List ✅
+// 💡 Task:
+// 1️⃣ Add tasks to a list when clicking a button.
+// 2️⃣ Remove a task when clicking on it.
+
+// 🎯 Next Steps
+// 1️⃣ Modify the To-Do List to store tasks in localStorage, so they remain after page refresh.
+// 2️⃣ Add a "Clear All" button to remove all tasks at once.
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    
+    document.getElementById("add-btn").addEventListener("click",function(){
+        let taskinput = document.getElementById("task-input").value;
+        
+        if(taskinput==="") return;
+        
+        let li = document.createElement("li");
+        li.textContent = taskinput;
+
+        //local storage stores in the form of string, thats why we wrote li.textContent
+        localStorage.setItem("Task : ",li.textContent);
+       
+        document.getElementById("task-list").append(li);
+
+        document.getElementById("clear-btn").addEventListener("click",function(){
+            li.remove(); // removes the task on click
+        });
+
+        
+        // we are clearing the input field so that when we click on add tasks button.The input field gets empty itself
+
+        document.getElementById("task-input").value="";
+        
+    });
+});
